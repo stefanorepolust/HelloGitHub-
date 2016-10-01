5cd5179fc4dad8213c7f0000c377980cfc8a5cc1
stefano Repolust <stefano.repolust@edu.campus02.at>
2016-10-01 11:04:50 +0200

    test commit

---
+++ src/HelloGitHub.java
@@ -0,0 +1,11 @@
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub!");
+		//this is a test
+	}
+
+}
