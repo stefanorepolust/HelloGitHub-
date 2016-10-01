bbd6bf530cb1fa233a88fa29bad37ebe0d55effb
stefanorepolust <stefano.repolust@edu.campus02.at>
2016-10-01 12:35:15 +0200

    test #4

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class HelloGitHub {
 
@@ -6,6 +7,28 @@ public class HelloGitHub {
 		
 		System.out.println("Hello GitHub!");
 		//this is a test asdf
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Bitte zahl 1 eingeben:");
+		int z1 = scanner.nextInt();
+		System.out.println("Bitte zahl 2 eingeben:");
+		int z2 = scanner.nextInt();
+		
+		int teiler = testcalc(z1, z2);
+		System.out.println("ggt:"+teiler);
+		
+//		System.out.println();
+	}
+	
+	private static int testcalc(int z1, int z2) {
+		   int r=0;
+		   while (z2 != 0) {
+		        r = z1 % z2;
+		        z1 = z2;
+		        z2 = r;
+		    }
+		   return z1;
+
 	}
 
 }
